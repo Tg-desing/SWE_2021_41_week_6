4df6c8c7a1c2e28df6ee12d336c10dfcc92779a7
Taegon <taegon@cusee.net>
2024-04-11 19:04:44 +0900

    Remove inappropriate test case code

--- main.py
+++ main.py
@@ -7,12 +7,3 @@ def twoSum(nums: List[int], target: int) -> List[int]:
                 return [i, j]
 
 
-if __name__ == "__main__":
-    try:
-        # please input list like "0 1 3 4" which seperate each element by space
-        input_list = list(map(int, input("Enter the list: ").split()))
-        input_target = int(input("Enter the target: "))
-        output_list = twoSum(input_list, input_target)
-        print("Output: " + str(output_list))
-    except ValueError:
-        print("Enter a integer number for list and target")
